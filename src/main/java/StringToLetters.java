b9d4332f804bea2f96c14a20b026f32edba3b195
SvetlanaG1011 <Sg26025959@gmail.com>
2022-08-05 21:25:06 -0800

    Algorithm stringToLetters created

---
+++ src/main/java/StringToLetters.java
@@ -0,0 +1,18 @@
+public class StringToLetters {
+
+    public String stringToLettersAlgorithm(String str) {
+        String result = "";
+
+        if (str.length() != 0) {
+            for (int i = 0; i < str.length(); i++){
+                if (str.charAt(i) >= 65 &&str.charAt(i) <= 90 || str.charAt(i) >= 97 && str.charAt(i) <= 122) {
+                    result+= str.charAt(i);
+                }
+            }
+
+            return result;
+        }
+
+        return "";
+    }
+}
