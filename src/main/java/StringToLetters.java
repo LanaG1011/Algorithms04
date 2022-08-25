29e1de3ada00da435425b2afa711bfe4a19f763f
SvetlanaG1011 <Sg26025959@gmail.com>
2022-08-24 20:55:35 -0800

    Algorithm stringToLetters changed

--- src/main/java/StringToLetters.java
+++ src/main/java/StringToLetters.java
@@ -3,15 +3,15 @@ public class StringToLetters {
     public String stringToLettersAlgorithm(String str) {
         String result = "";
 
-        if (str.length() != 0) {
-            for (int i = 0; i < str.length(); i++){
-                if (str.charAt(i) >= 65 &&str.charAt(i) <= 90 || str.charAt(i) >= 97 && str.charAt(i) <= 122) {
-                    result+= str.charAt(i);
+            if (str != null && str.length() != 0) {
+                for (int i = 0; i < str.length(); i++) {
+                    if (str.charAt(i) >= 65 && str.charAt(i) <= 90 || str.charAt(i) >= 97 && str.charAt(i) <= 122) {
+                        result += str.charAt(i);
+                    }
                 }
-            }
 
-            return result;
-        }
+                return result;
+            }
 
         return "";
     }
