d3ddad7e1bc87f1e40d5e532055ad92031d82ffc
SvetlanaG1011 <Sg26025959@gmail.com>
2022-10-01 00:42:48 -0800

    peakElementAlgorithm created

--- src/main/java/PeakElement.java
+++ src/main/java/PeakElement.java
@@ -1,2 +1,34 @@
-package PACKAGE_NAME;public class PeakElement {
+public class PeakElement {
+
+    public int[] peakElementAlgorithm(int[] array) {
+        int length = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            if (i == 0 && array[0] > array[1]) {
+                length++;
+            } else if (i > 0 && i < array.length - 1 && array[i] > array[i - 1] && array[i] > array[i + 1]) {
+                length++;
+            } else if (array[array.length - 1] > array[array.length - 2]) {
+                length++;
+            }
+        }
+
+        int[] result = new int[length];
+        int index = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            if (i == 0 && array[0] > array[1]) {
+                result[index] = array[0];
+                index++;
+            } else if (i > 0 && i < array.length - 1 && array[i] > array[i - 1] && array[i] > array[i + 1]) {
+                result[index] = array[i];
+                index++;
+            } else if (array[array.length - 1] > array[array.length - 2]) {
+                result[index] = array[array.length - 1];
+                index++;
+            }
+        }
+
+        return result;
+    }
 }
